e45124bbefde2219ba5392fe7c155c4c954a3e3f
emersonbusson <emersonbusson@gmail.com>
2025-08-29 17:37:37 -0300

    refactor: standardize database cleanup

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,9 +1,8 @@
-import database from "infra/database";
 import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("drop schema public cascade; create schema public");
+  await orchestrator.clearDatabase();
 });
 
 describe("When: I send a POST request to /api/v1/migrations", () => {
